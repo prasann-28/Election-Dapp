917d76999872b7d96adc8b7fce9fa595fe5ea742
Prasann Shimpi <prasann.shimpi@icloud.com>
2020-10-10 16:01:31 +0530

    front end components

--- pages/login.js
+++ pages/login.js
@@ -5,10 +5,21 @@ import Web3 from 'web3'
 import Link from 'next/link'
 
 export default function Login(){
+   
+    //functions to link=> 1.authenticate 
+    //grab id and password from user and pass it to authenticate
+    
+    
+   
+   
+   
+   
     return(
         <>
         <Head><title>Login</title></Head>
         <div><h2>Hello voter</h2></div>
         </>
     )
+   
+   
 }
