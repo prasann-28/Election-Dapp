88280b50532d20c4af597166b78b588383a104ed
Satyam Asthana <47237196+satyamasthanaa@users.noreply.github.com>
2020-12-11 09:54:02 +0530

    firebase

---
+++ pages/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config={
+    apiKey: "AIzaSyB87noD2rauil9yHJlXVoBnEHkJtN1H7Bk",
+    authDomain: "reactotp-9d06d.firebaseapp.com",
+    projectId: "reactotp-9d06d",
+    storageBucket: "reactotp-9d06d.appspot.com",
+    messagingSenderId: "409884613297",
+    appId: "1:409884613297:web:236d420c45c00fe7a1e090"
+}
+if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+export default firebase 
