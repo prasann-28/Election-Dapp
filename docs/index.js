01c1fb77c6529bc139bb9cd4218a8095211c4fb5
Prasann Shimpi <prasannshimpi@Prasanns-MacBook-Pro.local>
2021-05-23 16:36:26 +0530

    docs

---
+++ docs/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import LoginButton from './api/LoginButton'
+import ContainerExampleText from './api/container'
+import EHeader from './api/HeaderLogin'
+import Head from 'next/head'
+import {Icon} from 'semantic-ui-react'
+import 'semantic-ui-css/semantic.min.css'
+
+export default function Home() {
+  return (
+   <>
+   <Head><title>Welcome</title></Head>
+    <div className='Home'><EHeader /></div>
+   <body className='intro'>
+     {/* <ContainerExampleText/> */}
+    <LoginButton/>
+   </body>
+   </>
+  )
+}
